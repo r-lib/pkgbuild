97490fb2f9e754e7c2e735262a74dd14fe11234a
hadley <h.wickham@gmail.com>
2016-11-09 10:18:17 -0600

    And it makes sense for compile_dll to live here

---
+++ R/install-min.r
@@ -0,0 +1,26 @@
+install_min <- function(pkg = ".", dest, components = NULL, args = NULL, quiet = FALSE) {
+  pkg <- as.package(pkg)
+  stopifnot(is.character(dest), length(dest) == 1, file.exists(dest))
+
+  poss <- c("R", "data", "help", "demo", "inst", "docs", "exec", "libs")
+  if (!is.null(components)) {
+    components <- match.arg(components, poss, several.ok = TRUE)
+  }
+  no <- setdiff(poss, components)
+  no_args <- paste0("--no-", no)
+
+  callr::rcmd(
+    "INSTALL",
+    c(
+      shQuote(pkg$path),
+      paste("--library=", shQuote(dest), sep = ""),
+      no_args,
+      "--no-multiarch",
+      "--no-test-load",
+      args
+    ),
+    show = !quiet
+  )
+
+  invisible(file.path(dest, pkg$package))
+}
