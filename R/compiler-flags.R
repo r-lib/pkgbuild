9ff56d593af605a1b03d3af067869d8874b35a50
hadley <h.wickham@gmail.com>
2016-11-09 10:14:52 -0600

    Also need compiler flags

---
+++ R/compiler-flags.R
@@ -0,0 +1,37 @@
+#' Default compiler flags used by devtools.
+#'
+#' These default flags enforce good coding practice by ensuring that
+#' \env{CFLAGS} and \env{CXXFLAGS} are set to \code{-Wall -pedantic}.
+#' These tests are run by cran and are generally considered to be good practice.
+#'
+#' By default \code{\link{compile_dll}} is run with \code{compiler_flags(TRUE)},
+#' and check with \code{compiler_flags(FALSE)}.  If you want to avoid the
+#' possible performance penalty from the debug flags, install the package.
+#'
+#' @param debug If \code{TRUE} adds \code{-g -O0} to all flags
+#'   (Adding \env{FFLAGS} and \env{FCFLAGS}
+#' @family debugging flags
+#' @export
+#' @examples
+#' compiler_flags()
+#' compiler_flags(TRUE)
+compiler_flags <- function(debug = FALSE) {
+  if (Sys.info()[["sysname"]] == "SunOS") {
+    c(
+      CFLAGS   = "-g",
+      CXXFLAGS = "-g"
+    )
+  } else if (debug) {
+    c(
+      CFLAGS   = "-UNDEBUG -Wall -pedantic -g -O0",
+      CXXFLAGS = "-UNDEBUG -Wall -pedantic -g -O0",
+      FFLAGS   = "-g -O0",
+      FCFLAGS  = "-g -O0"
+    )
+  } else {
+    c(
+      CFLAGS   = "-Wall -pedantic",
+      CXXFLAGS = "-Wall -pedantic"
+    )
+  }
+}
