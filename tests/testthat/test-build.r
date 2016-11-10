558f5f1ec9c94d9ab04c76c37c238b9da6de4592
hadley <h.wickham@gmail.com>
2016-11-10 10:12:13 -0600

    Add tests for build

---
+++ tests/testthat/test-build.r
@@ -0,0 +1,11 @@
+context("build")
+
+test_that("source builds return correct filenames", {
+  path <- build("testNamespace", dest_path = tempdir(), quiet = TRUE)
+  expect_true(file.exists(path))
+})
+
+test_that("binary builds return correct filenames", {
+  path <- build("testNamespace", binary = TRUE, dest_path = tempdir(), quiet = TRUE)
+  expect_true(file.exists(path))
+})
