0ddc926d1dc7436e0a10e11e5fa477682d9e51b8
hadley <h.wickham@gmail.com>
2016-11-10 13:25:36 -0600

    Eliminate package dep in test package

--- tests/testthat/test-build.r
+++ tests/testthat/test-build.r
@@ -1,14 +1,14 @@
 context("build")
 
 test_that("source builds return correct filenames", {
-  path <- build("testNamespace", dest_path = tempdir(), quiet = TRUE)
+  path <- build("testDummy", dest_path = tempdir(), quiet = TRUE)
   on.exit(unlink(path))
 
   expect_true(file.exists(path))
 })
 
 test_that("binary builds return correct filenames", {
-  path <- build("testNamespace", binary = TRUE, dest_path = tempdir(), quiet = TRUE)
+  path <- build("testDummy", binary = TRUE, dest_path = tempdir(), quiet = TRUE)
   on.exit(unlink(path))
 
   expect_true(file.exists(path))
