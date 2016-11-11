a55466a597500ff2d8e75662f29746c769e42792
hadley <h.wickham@gmail.com>
2016-11-11 09:34:24 -0600

    Add src dir to git

---
+++ tests/testthat/testWithSrc/src/add1.c
@@ -0,0 +1,11 @@
+// In C ----------------------------------------
+#include <R.h>
+#include <Rinternals.h>
+
+SEXP add1(SEXP a) {
+  SEXP result = PROTECT(allocVector(REALSXP, 1));
+  REAL(result)[0] = asReal(a) + 1;
+  UNPROTECT(1);
+
+  return result;
+}
