86113eb954d767701df02397d4c8552293fa0773
hadley <h.wickham@gmail.com>
2016-11-11 16:52:33 -0600

    More debugging. Tweak logic

--- R/rtools-path.R
+++ R/rtools-path.R
@@ -1,20 +1,10 @@
-scan_path_for_rtools <- function(debug = FALSE,
-                                 gcc49 = using_gcc49(),
-                                 arch = gcc_arch()) {
-  if (debug)
-    cat("Scanning path...\n")
+scan_path_for_rtools <- function(debug = FALSE) {
 
-  # First look for ls and gcc
-  ls_path <- Sys.which("ls")
-  if (ls_path == "") return(NULL)
-  if (debug)
-    cat("ls:", ls_path, "\n")
+  if (using_gcc49()) {
+    # First check if gcc set by BINPREF/CC is valid and use that if so
+    if (debug)
+      cat("Trying R CMD config CC...\n")
 
-  # We have a candidate installPath
-  install_path <- dirname(dirname(ls_path))
-
-  if (gcc49) {
-    # First check if gcc set by BINPREF/CC is valid and use that is so
     cc_path <- callr::rcmd_safe("config", "CC")$stdout
     # remove '-m64' from tail if it exists
     cc_path <- sub("[[:space:]]+-m[[:digit:]]+$", "", cc_path)
@@ -22,33 +12,44 @@ scan_path_for_rtools <- function(debug = FALSE,
     if (debug)
       cat("cc_path:", cc_path, "\n")
 
-    gcc_path <- find_gcc49(cc_path, debug = debug)
-    if (nzchar(gcc_path)) {
-      return(rtools(install_path, NULL, valid_binpref = TRUE))
+    gcc_path <- find_arch_exe(cc_path, debug = debug)
+    if (gcc_path != "") {
+      return(rtools(gcc_path, NULL, valid_binpref = TRUE))
     }
+  }
 
-    # if not check default location Rtools/mingw_{32,64}/bin/gcc.exe
-    if (debug)
-      cat("Trying default location\n")
-    gcc_path <- find_gcc49(
-      file.path(install_path, paste0("mingw_", arch), "bin", "gcc.exe"),
+  if (debug)
+    cat("Scanning path...\n")
+
+  # Next looks for ls and gcc on path
+  ls_path <- Sys.which("ls")
+  if (ls_path == "")
+    return(NULL)
+  if (debug)
+    cat("ls:", ls_path, "\n")
+
+  # We have a candidate install_path
+  install_path <- dirname(dirname(ls_path))
+
+  gcc_path <- Sys.which("gcc")
+  if (debug)
+    cat("gcc_path:", gcc_path, "\n")
+
+  if (gcc_path != "") {
+    # Check both candidate install paths are same
+    install_path2 <- dirname(dirname(dirname(gcc_path)))
+    if (tolower(install_path2) != tolower(install_path))
+      return(NULL)
+  } else {
+    # Maybe isn't on path, but is in default location
+    gcc_default <- find_arch_exe(
+      file.path(install_path, paste0("mingw_", gcc_arch()), "bin", "gcc.exe"),
       debug = debug
     )
-    if (!nzchar(gcc_path)) {
+    if (gcc_default == "") {
       return(NULL)
     }
-  } else {
-    gcc_path <- Sys.which("gcc")
-    if (gcc_path == "")
-      return(NULL)
   }
-  if (debug)
-    cat("gcc:", gcc_path, "\n")
-
-  install_path2 <- dirname(dirname(dirname(gcc_path)))
-
-  # If both install_paths are not equal
-  if (tolower(install_path2) != tolower(install_path)) return(NULL)
 
   version <- installed_version(install_path, debug = debug)
   if (debug)
@@ -57,19 +58,20 @@ scan_path_for_rtools <- function(debug = FALSE,
   rtools(install_path, version)
 }
 
-find_gcc49 <- function(path, debug = FALSE) {
+find_arch_exe <- function(path, debug = FALSE) {
+  # First try adding .exe
   if (!file.exists(path)) {
     path <- paste0(path, ".exe")
   }
+
+  if (!file.exists(path))
+    return("")
+
+  # Then check architecture matches
   file_info <- file.info(path)
+  if (file_info$exe != paste0("win", gcc_arch()))
+    return("")
 
-  # file_info$exe should be win32 or win64 respectively
-  if (debug)
-    cat("cc_exe: ", file_info$exe)
-
-  if (!file.exists(path) || file_info$exe != paste0("win", arch)) {
-    return(character(1))
-  }
   path
 }
 
