3dc41185e9e6ac994caa66ea7651a571c042b6f4
Jim Hester <james.f.hester@gmail.com>
2018-02-12 17:06:03 -0500

    set quiet = FALSE to see the error

--- tests/testthat/test-compile_dll.R
+++ tests/testthat/test-compile_dll.R
@@ -1,7 +1,7 @@
 context("compile_dll")
 
 test_that("can compile a DLL and clean up afterwards", {
-  expect_error(compile_dll("testWithSrc", quiet = TRUE), NA)
+  expect_error(compile_dll("testWithSrc", quiet = FALSE), NA)
 
   clean_dll("testWithSrc")
   expect_equal(dir("testWithSrc/src"), "add1.c")
