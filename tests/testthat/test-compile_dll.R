2b4be8e2c327a660880f92ffeb95550e852b0bae
Jim Hester <james.f.hester@gmail.com>
2018-03-15 11:37:43 -0400

    Quiet output from test, rename context

--- tests/testthat/test-compile_dll.R
+++ tests/testthat/test-compile_dll.R
@@ -1,7 +1,7 @@
-context("compile_dll")
+context("test-compile_dll.R")
 
 test_that("can compile a DLL and clean up afterwards", {
-  expect_error(compile_dll("testWithSrc", quiet = FALSE), NA)
+  expect_error(compile_dll("testWithSrc", quiet = TRUE), NA)
 
   clean_dll("testWithSrc")
   expect_equal(dir("testWithSrc/src"), "add1.c")
