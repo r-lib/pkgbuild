05b696a64680f949b512df9a23a77f6fcdb0cfa3
dgkf <18220321+dgkf@users.noreply.github.com>
2021-11-03 18:03:54 -0700

    vignettes inst/doc clean respects build args

--- R/build.R
+++ R/build.R
@@ -125,7 +125,11 @@ build_setup_source <- function(path, dest_path, vignettes, manual, clean_doc,
   }
 
   if (!manual) {
-    args <- c(args, "--no-manual")
+    args <- unique(c(args, "--no-manual"))
+  }
+
+  if (!vignettes) {
+    args <- unique(c(args, "--no-build-vignettes"))
   }
 
   no_manual <- "--no-manual" %in% args
@@ -137,9 +141,8 @@ build_setup_source <- function(path, dest_path, vignettes, manual, clean_doc,
     check_build_tools(quiet = TRUE)
   }
 
-  if (!vignettes) {
-    args <- c(args, "--no-build-vignettes")
-  } else if (is.null(clean_doc) || isTRUE(clean_doc)) {
+  build_vignettes <- !("--no-build-vignettes" %in% args)
+  if (build_vignettes && (is.null(clean_doc) || isTRUE(clean_doc))) {
     doc_dir <- file.path(path, "inst", "doc")
     if (dir.exists(doc_dir)) {
       if (is.null(clean_doc) && interactive()) {
