fa73d563b254606759656337f6338871d610600d
Marco Colombo <mar.colombo13@gmail.com>
2024-11-03 14:08:09 +0100

    Fix appearance of nested backticks in man page.

--- R/compile-dll.R
+++ R/compile-dll.R
@@ -34,7 +34,7 @@
 #' @note If this is used to compile code that uses Rcpp, you will need to
 #'   add the following line to your `Makevars` file so that it
 #'   knows where to find the Rcpp headers:
-#'   `PKG_CPPFLAGS=`$(R_HOME)/bin/Rscript -e 'Rcpp:::CxxFlags()'``
+#'   \verb{PKG_CPPFLAGS=`$(R_HOME)/bin/Rscript -e 'Rcpp:::CxxFlags()'`}
 #'
 #' @inheritParams build
 #' @param force If `TRUE`, for compilation even if [needs_compile()] is
